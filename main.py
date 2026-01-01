44131e5d2159cf1560791a64f448a6237e83e50f
Kgaphola Emmanuel <It45ma@protonmail.ch>
2026-01-01 11:10:07 +0000

    feat: Initialized SQLite database and create a users table for authentication.

--- main.py
+++ main.py
@@ -3,3 +3,18 @@ import tkinter as tk
 from tkinter import messagebox
 from hashlib import sha256
 import csv
+
+
+#DATABASE
+conn = sqlite3.connect("load_shedding.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE,
+    password TEXT,
+    area TEXT
+)
+""")
+conn.commit()
