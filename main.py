701038a8c26135f00e8ff75ff3cca4386fa7764b
Kgaphola Emmanuel <It45ma@protonmail.ch>
2026-01-01 11:11:09 +0000

    feat: Implemented loading of the load shedding schedule from a CSV file.

--- main.py
+++ main.py
@@ -18,3 +18,24 @@ CREATE TABLE IF NOT EXISTS users (
 )
 """)
 conn.commit()
+
+
+
+# loading the CSV
+def load_schedule_from_csv(filename="load_shedding_schedule.csv"):
+    schedule = {}
+    try:
+        with open(filename, newline="", encoding="utf-8") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                area = row["area"]
+                time_slot = row["time_slot"]
+
+                if area not in schedule:
+                    schedule[area] = []
+                schedule[area].append(time_slot)
+    except FileNotFoundError:
+        messagebox.showerror("Error", "load_shedding_schedule.csv not found!")
+    return schedule
+
+mock_schedule = load_schedule_from_csv()
